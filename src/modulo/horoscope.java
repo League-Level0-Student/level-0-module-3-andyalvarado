b64c43c0856e6029f46fe53f6d3cb1cd778fe532
ilabstaff <ilabstaff@172.16.116.77>
2019-03-19 17:44:46 -0700

    horoscopeyearsalive

---
+++ src/modulo/horoscope.java
@@ -0,0 +1,120 @@
+package modulo;
+
+import javax.swing.JOptionPane;
+
+public class horoscope {
+
+public static void main(String[] args) {
+
+String too =JOptionPane.showInputDialog(null,"What is your Star sign?");
+
+
+if (too.equals ("Aquarius")) { 
+JOptionPane.showMessageDialog(null,"you are really friendly");	
+}
+if (too.equals ("pisces")) { 
+JOptionPane.showMessageDialog(null,"you are empathatic ");	
+}
+if (too.equals ("cancer")) { 
+JOptionPane.showMessageDialog(null,"you are very humorous");	
+}
+
+if (too.equals ("gemini")) { 
+JOptionPane.showMessageDialog(null,"you are great very charming");	
+}
+if (too.equals ("leo")) { 
+JOptionPane.showMessageDialog(null,"you are honest");	
+}
+if (too.equals ("sagittarias")) { 
+JOptionPane.showMessageDialog(null,"you are really intellegent");	
+}
+if (too.equals ("capricorns")) { 
+JOptionPane.showMessageDialog(null,"you are really logical");	
+}
+if (too.equals ("tauras")) { 
+JOptionPane.showMessageDialog(null,"you finish what you start");	
+}
+if (too.equals ("aries")) { 
+JOptionPane.showMessageDialog(null,"you are really energetic");	
+}
+if (too.equals ("virgo")) { 
+JOptionPane.showMessageDialog(null,"you are really kind ");	
+}
+if (too.equals ("libra")) { 
+JOptionPane.showMessageDialog(null,"you are really fair");	
+}
+if (too.equals ("scorpio")) { 
+JOptionPane.showMessageDialog(null,"you have self control ");	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}	
+	
+}
