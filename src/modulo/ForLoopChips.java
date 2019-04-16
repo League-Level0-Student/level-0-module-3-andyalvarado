20fd4fbd4316bc43e1be7be5bffb9e9b97c9ade1
ilabstaff <ilabstaff@172.16.116.68>
2019-04-15 17:29:29 -0700

    forloop

---
+++ src/modulo/ForLoopChips.java
@@ -0,0 +1,28 @@
+package modulo;
+
+public class ForLoopChips {
+	/*
+	 * 
+	 * 1 frito
+	 * 2 chito
+	 * 3 dorito
+	 * */
+	
+	public static void main(String[] args) {
+for (int i = 1; i <4 ; i++) {
+System.out.print(i);
+if (i==1) {
+	System.out.println(" frito");
+}
+else if (i==2) {
+System.out.println(" chito");	
+}if (i==3) {
+System.out.println(" dorito");	
+}
+	
+
+}	
+	
+	
+}
+}
--- src/modulo/checkpoint3.java
+++ src/modulo/checkpoint3.java
@@ -5,11 +5,20 @@ import javax.swing.JOptionPane;
 public class checkpoint3 {
 public static void main(String[] args) {
 for (int i = 1; i <9; i++) {
-System.out.println(i+" potato");	
-
+	
+if (i==4) {
+	System.out.println(4);
 
 }
+else if (i==8) {
+	System.out.println("more");	
 
+
+}else {
+	System.out.println(i+" potato,");	
+}
+
+}
 
 
 
